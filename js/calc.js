78e91b1c9ff0a8c7947ba9a19f0c0e3f7184ceda
Maks Martynov <maks.martynv@gmail.com>
2015-01-11 21:32:48 +0200

    Simple test

--- js/calc.js
+++ js/calc.js
@@ -3,6 +3,30 @@ function Calc(){
 
 Calc.prototype = {
 	solve: function(expr){
-		return expr;
+		if(typeof expr === 'string')
+		{
+			if (expr.match(/^[0-9+\-*/(). ]*$/))
+			{
+				try
+				{
+					var answer = expr != '' ? eval(expr) : '0';
+
+
+					return answer;
+				}
+				catch (e)
+				{
+					return e;
+				}
+			}
+			else
+			{
+				return 'Input parameter not looks like the expression';
+			}
+		}
+		else
+		{
+			return 'Input parameter must be value of string type';
+		}
 	}
 }
