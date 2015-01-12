1f0115d154d0ab047c53ae343585208d2652dadb
Maks Martynov <maks.martynv@gmail.com>
2015-01-13 00:54:30 +0200

    8 of 9

--- js/calc.js
+++ js/calc.js
@@ -2,35 +2,86 @@ function Calc(){
 }
 
 Calc.prototype = {
+
+	preDefinedRegex: /(\d+)/g,
+	preDefinedOperators : ['+','-','/','*',],
+	firstNumber: void 0,
+	secondNumber: void 0,
+	operatorMark: void 0,
+	preDefinedContains: false,
 	solve: function(expr){
 		if(typeof expr === 'string')
 		{
-			if (expr.match(/^[0-9+\-*/(). ]*$/))
-			{
 				try
 				{
-					var answer = expr != '' ? eval(expr) : '0';
+					var constantExpression = this.splitExpression(expr);
+					if(typeof constantExpression != 'undefined') return constantExpression;
+					if(this.preDefinedContains)
+					{
+						return this.handleBaseOperators()
+					}
+					else
+					{
+						var fn = Calc.prototype[this.operatorMark];
+						if(typeof fn === 'function') {
+							return fn(this.fistNumber,this.secondNumber);
+						}
+					}
 
-
-					return answer;
 				}
 				catch (e)
 				{
 					throw e;
 				}
-			}
-			else
-			{
-				throw 'input parameter not looks like the expression';
-			}
+
 		}
 		else
 		{
 			throw 'input parameter must be value of string type';
 		}
 	},
-	defineOperator:function(expr)
-	{
-		return expr;
+	defineOperator:function(operatorName, fn) {
+			if(this.preDefinedOperators.indexOf(operatorName) === -1)
+			{
+				if(typeof fn == 'function')
+				{
+					Calc.prototype[ operatorName ] =  fn;
+				}
+			}
+			else
+			{
+				throw 'unable to replace default operator';
+			}
+
+	},
+	handleBaseOperators: function(){
+		switch (this.operatorMark){
+			case "+": return this.fistNumber+this.secondNumber;
+				break;
+			case "-": return this.fistNumber-this.secondNumber;
+				break;
+			case "/": return this.fistNumber/this.secondNumber;
+				break;
+			case "*": return this.fistNumber*this.secondNumber;
+				break;
+			default:
+				break;
+
+		}
+	},
+	splitExpression: function(expr){
+		var expressionArray = expr.split(this.preDefinedRegex);
+		expressionArray.splice(0, 1);
+		expressionArray.splice(expressionArray.length-1, 1);
+
+		this.preDefinedContains = expressionArray.some(function(entry) {
+			if(entry) return Calc.prototype.preDefinedOperators.indexOf(entry) != -1
+
+		});
+		if(expressionArray.length===1) return eval(expr);
+		this.fistNumber = parseInt(expressionArray[0]);
+		this.secondNumber = parseInt(expressionArray[2]);
+		this.operatorMark = expressionArray[1];
+
 	}
 }
